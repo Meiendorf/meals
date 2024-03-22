08028a6dd2732acb06199e1eab0569530f449b34
meiendorf <neo1110111@gmail.com>
2024-03-23 01:12:25 +0200

    Add category-specific meals to MealsScreen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/models/meal.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    List<Meal> meals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (ctx) => MealsScreen(
-          title: 'Some title..',
-          meals: [],
+          title: category.title,
+          meals: meals,
         ),
       ),
     );
@@ -35,7 +41,7 @@ class CategoriesScreen extends StatelessWidget {
           for (final category in availableCategories)
             CategoryGridItem(
               category: category,
-              onSelectCategory: () => _selectCategory(context),
+              onSelectCategory: () => _selectCategory(context, category),
             )
         ],
       ),

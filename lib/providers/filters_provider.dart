63bda82f95a7b8cef09b83b18271245d07361a78
meiendorf <neo1110111@gmail.com>
2024-03-28 01:26:00 +0200

    Refactor meal filtering logic and update meal details screen

--- lib/providers/filters_provider.dart
+++ lib/providers/filters_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/providers/meals_provider.dart';
 
 enum Filter {
   glutenFree,
@@ -31,3 +32,18 @@ class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
 final filtersProvider =
     StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
         (ref) => FiltersNotifier());
+
+final filteredMealsProvider = Provider((ref) {
+  final meals = ref.watch(mealsProvider);
+  final activeFilters = ref.watch(filtersProvider);
+  return meals.where((meal) {
+    if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree ||
+        activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree ||
+        activeFilters[Filter.vegan]! && !meal.isVegan ||
+        activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+      return false;
+    } else {
+      return true;
+    }
+  }).toList();
+});
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -5,7 +5,6 @@ import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/main_drawer.dart';
-import 'package:meals/providers/meals_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class TabsScreen extends ConsumerStatefulWidget {
@@ -38,19 +37,7 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final meals = ref.watch(mealsProvider);
-    final activeFilters = ref.watch(filtersProvider);
-
-    final availableMeals = meals.where((meal) {
-      if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree ||
-          activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree ||
-          activeFilters[Filter.vegan]! && !meal.isVegan ||
-          activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
-        return false;
-      } else {
-        return true;
-      }
-    }).toList();
+    final availableMeals = ref.watch(filteredMealsProvider);
 
     Widget activePage = CategoriesScreen(
       availableMeals: availableMeals,
